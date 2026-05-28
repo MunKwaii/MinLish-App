b9dd14047c9b21f8f413f9858aa6edab4eab25f2
Khanh <Quockhanh6310@gmail.com>
2026-05-29 00:34:17 +0700

    Add Room entity and KSP/Room setup
    Introduce a Room User entity and configure KSP/Room dependencies. Adds a new User.kt @Entity data class for users. Updates app/build.gradle.kts to apply the KSP plugin and include room runtime/ktx plus ksp(room-compiler). Updates gradle/libs.versions.toml with room and ksp versions and library entries. Also adds .idea/vcs.xml and minor IDE project setting tweaks.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)                // plugin Ksp để biên dịch Room
 }
 
 android {
@@ -55,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // ROOM LIBRARY
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler) // Dùng ksp
 }
---
+++ app/src/main/java/vn/edu/hcmute/minlish/data/local/entity/User.kt
@@ -0,0 +1,21 @@
+package vn.edu.hcmute.minlish.data.local.entity
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val userId: Int = 0,
+
+    val email: String,
+    val name: String,
+    val passwordHash: String,
+    val learningGoal: String = "",
+    val level: String = "",
+
+    val totalWordsLearned: Int = 0,
+    val currentStreak: Int = 0,
+    val accuracyRate: Float = 0f
+)

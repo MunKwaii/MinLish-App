9979870fd49a19ec90802d94913f31a2cd2d2379
KickHuynh <huynhngoctai205@gmail.com>
2026-05-31 00:49:13 +0700

    update routes
    update routes

--- app/src/main/java/vn/edu/hcmute/minlish/ui/navigation/Routes.kt
+++ app/src/main/java/vn/edu/hcmute/minlish/ui/navigation/Routes.kt
@@ -5,4 +5,21 @@ sealed class Screen(val route: String) {
     object Register : Screen("register")
     object Dashboard : Screen("dashboard")
     object Profile : Screen("profile")
+
+    // Màn hình danh sách bộ từ vựng
+    object Vocabulary : Screen("vocabulary")
+
+    // Màn hình danh sách từ vựng theo deckId
+    object WordList : Screen("word_list/{deckId}") {
+        fun createRoute(deckId: Int): String {
+            return "word_list/$deckId"
+        }
+    }
+
+    // Màn hình thêm từ vựng theo deckId
+    object AddWord : Screen("add_word/{deckId}") {
+        fun createRoute(deckId: Int): String {
+            return "add_word/$deckId"
+        }
+    }
 }
